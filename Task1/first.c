c7e16ac1adaff894238a39e1aafd805f8cadd9cb
Yuriy Perezhogin <wolfviking@yandex.ru>
2024-06-30 10:13:31 +0500

    add Task1/first

---
+++ Task1/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    num = 1;
+
+    printf("Я простой ");
+    printf("компьютер.\n");
+    printf("Моей любимой цифрой является %d, так как она первая.\n", num);
+
+    return 0;
+}
